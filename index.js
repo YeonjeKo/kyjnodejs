5c28a1d54736e5fa5eeb6333784dc039f3432ad5
kyounje <kyounje@naver.com>
2020-10-21 22:41:19 +0900

    protect secret info

--- index.js
+++ index.js
@@ -1,13 +1,36 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser');;
+const config = require('./config/key');
+//const { User } = request("./models/User");
+
+//application/x-www-form0urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+
+//application/json
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://kyounje:qnldkv9350@kyjnodejs.hkqfs.mongodb.net/kyjnodejs?retryWrites=true&w=majority', {
+mongoose.connect(config.mongoURI, {
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log('MongoDB Conneted...'))
   .catch(err => console.log(err))
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello World! haha'))
+
+app.post('/register', (req, res) => {
+  //회원가입할 때 필요한 정보들을 client에서 가져오면
+  //그것들을 db에 넣어준다
+
+  const user = new User(req.body)
+
+  user.save((err, userInfo) => {
+    if(err) return res.json({ success: false, err})
+    return res.status(200).json({
+      success: true
+    })
+  })
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
